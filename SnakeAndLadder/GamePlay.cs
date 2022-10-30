9ec000063ee0ce118425fefae62b5cca6fb9fc89
PrajaktaShewale <shewalep94@gmail.com>
2022-10-30 23:48:49 +0530

    [Prajakta]Add:PlayedwithSinglePlayer

---
+++ SnakeAndLadder/GamePlay.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    internal class GamePlay
+    {
+        public void Game()
+        {
+          int PlayerPosition = 0;
+            Console.WriteLine("Game is Start.Player is at starting position:"+PlayerPosition);
+        }
+    }
+}
