95fe29a2e8879476c76caee50a2f2507b4efcca0
PrajaktaShewale <shewalep94@gmail.com>
2022-10-31 00:50:09 +0530

    [Prajakta]Add:CheckForOption

--- SnakeAndLadder/GamePlay.cs
+++ SnakeAndLadder/GamePlay.cs
@@ -1,15 +1,32 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace SnakeAndLadder
 {
-    public class GamePlay
+    internal class GamePlay
     {
-        
+        int Position = 0;
         Random random = new Random();
         public void DieRoll()
         {
-          int die = random.Next(1,7);
-            Console.WriteLine(die);
+            int num = random.Next(1, 7);
+            int num1 = random.Next(0, 3);
+           switch (num1)
+        
+           {
+                case 0:
+                    Console.WriteLine("Player is in the same position");
+                    break;
+                case 1:
+                    Position += num;
+                    Console.WriteLine("Player position is : " + Position);
+            break;
+                case 2:
+                    Position -= num;
+                    Console.WriteLine("Player position is : " + Position);
+                    break;
+            }
         }
+        
     }
 }
