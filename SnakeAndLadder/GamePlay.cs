a74763706790a21e805558ea3e94ed3680305f47
PrajaktaShewale <shewalep94@gmail.com>
2022-10-31 01:02:05 +0530

    [Prajakta]Add:RestartAfterFinalPosition

--- SnakeAndLadder/GamePlay.cs
+++ SnakeAndLadder/GamePlay.cs
@@ -9,24 +9,32 @@ namespace SnakeAndLadder
         Random random = new Random();
         public void DieRoll()
         {
-            int num = random.Next(1, 7);
-            int num1 = random.Next(0, 3);
-           switch (num1)
-        
-           {
-                case 0:
-                    Console.WriteLine("Player is in the same position");
-                    break;
-                case 1:
-                    Position += num;
-                    Console.WriteLine("Player position is : " + Position);
-            break;
-                case 2:
-                    Position -= num;
-                    Console.WriteLine("Player position is : " + Position);
-                    break;
+            while (Position < 101)
+            {
+                int num = random.Next(1, 7);
+                int num1 = random.Next(0, 3);
+                switch (num1)
+
+                {
+                    case 0:
+                        Console.WriteLine("Player is in the same position");
+                        break;
+                    case 1:
+                        Position += num;
+                        Console.WriteLine("Player position is : " + Position);
+                        break;
+                    case 2:
+                        Position -= num;
+                        Console.WriteLine("Player position is : " + Position);
+                        if (Position < 0)
+                        {
+                            Position = 0;
+                            Console.WriteLine("Player position change to 0.So the player again Restart.");
+                        }
+                        break;
+                }
             }
+
         }
-        
     }
 }
