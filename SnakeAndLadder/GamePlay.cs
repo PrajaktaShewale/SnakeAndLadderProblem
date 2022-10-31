ff837351ea267f4ab453cf6f7319c41ecc40b722
PrajaktaShewale <shewalep94@gmail.com>
2022-10-31 12:40:33 +0530

    [Prajakta]Add:DiceNumAndPosition

--- SnakeAndLadder/GamePlay.cs
+++ SnakeAndLadder/GamePlay.cs
@@ -1,60 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace SnakeAndLadder
 {
     internal class GamePlay
     {
-        int Position = 0;
+        int position = 0, count = 0;
         Random random = new Random();
+        //UC3
         public void DieRoll()
         {
-            while (Position < 100)
+            while (position < 100)
             {
                 int num = random.Next(1, 7);
-                int num1 = random.Next(0, 3);
-                switch (num1)
-
+                int check = random.Next(0, 3);
+                switch (check)
                 {
                     case 0:
                         Console.WriteLine("Player is in the same position");
+                        count++;
                         break;
                     case 1:
-                        if (Position >93)
+                        count++;
+                        if (position > 93)
                         {
-                            int hundred = 100 - Position;
-                            Console.WriteLine("Game is in End Phase and position is :" + Position);
+                            int hundred = 100 - position;
+                            Console.WriteLine("Game is in the LAST PHASE and position is :" + position);
                             if (hundred >= num)
-                                {
-                                    Position += num;
-                                    Console.WriteLine("Player position is : " + Position);
-                                }
-                            else 
                             {
-                                Console.WriteLine("Wait For the Next Round Until get exact 100");
-                            }
+                                position += num;
+                                Console.WriteLine("Player position is : " + position);
                             }
                             else
                             {
-                                Position += num;
-                                Console.WriteLine("Player position is : " + Position);
+                                Console.WriteLine("Wait For the Next Round Until you get the exact 100");
                             }
-                            break;
-                    case 2:
-                        if (Position < 93)
+                        }
+                        else
                         {
+                            position += num;
+                            Console.WriteLine("Player position is : " + position);
+                        }
+
+                        break;
+                    case 2:
+                        count++;
+                        if (position > 93)
+                        {
+                            Console.WriteLine("Snake or Negative part is end");
                             break;
                         }
-                        Position -= num;
-                        Console.WriteLine("Player position is : " + Position);
-                        if (Position < 0)
+                        position -= num;
+                        Console.WriteLine("Player position is : " + position);
+                        if (position < 0)
                         {
-                            Position = 0;
+                            position = 0;
                             Console.WriteLine("Player position change to 0 . So the player again Restart From O");
                         }
                         break;
                 }
             }
-
+            Console.WriteLine("total number of DICE roll to win a game is " + count);
         }
     }
 }
-                        
